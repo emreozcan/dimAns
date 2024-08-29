1f385848daf9dc5b77940d2ee0fcae94fcb6f420
Emre Özcan <emre@emreis.com>
2024-08-29 19:39:08 +0300

    Units: Change symbols of all pint units from "pt" to "pint"
    This is done to distinguish from points.

--- src/dimans/units/imperial/volume.py
+++ src/dimans/units/imperial/volume.py
@@ -5,7 +5,7 @@ gallon = _DerivedUnit.using(_millilitre, "gal", (454609 / 100))
 
 fluid_ounce = _DerivedUnit.using(gallon, "fl_oz", (1 / 160))
 gill = _DerivedUnit.using(fluid_ounce, "gi", 5)
-pint = _DerivedUnit.using(fluid_ounce, "pt", 20)
+pint = _DerivedUnit.using(fluid_ounce, "pint", 20)
 quart = _DerivedUnit.using(fluid_ounce, "qt", 40)
 
 # British apothecaries' volume measures
