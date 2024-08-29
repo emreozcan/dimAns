8da33b7939cbe80358c7470fb13de2ebfebab70c
Emre Özcan <emre@emreis.com>
2024-08-29 19:38:15 +0300

    Units: Change symbols of US units

--- src/dimans/units/us/mass.py
+++ src/dimans/units/us/mass.py
@@ -3,8 +3,8 @@ from ..imperial.mass import grain as _grain, pound as _pound
 
 dram = _BaseUnit.using(_grain, "dr", (875 / 32))
 
-us_hundredweight = _BaseUnit.using(_pound, "cwt", 100)
-us_ton = _BaseUnit.using(us_hundredweight, "ton", 20)
+us_hundredweight = _BaseUnit.using(_pound, "US cwt", 100)
+us_ton = _BaseUnit.using(us_hundredweight, "US ton", 20)
 
 pennyweight = _BaseUnit.using(_grain, "dwt", 24)
 troy_ounce = _BaseUnit.using(pennyweight, "oz t", 20)
